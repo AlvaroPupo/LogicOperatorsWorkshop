a19c8baa9062c08b0318283e944b45197c52ae9b
AlvaroPupo <juanhernandezpupo@gmail.com>
2018-01-24 16:35:23 -0500

    AND example

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,26 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+    /*
+    > greater than
+    < less than
+    >= greater than or equal to
+    <= less than or equal to
+    != not equal to
+    == check one value against another
+    ! not
+    && and
+    ||
+     */
 
+    int a = 2;
+    int b = 4;
+    int x = 6;
+    int y = 1;
+
+    if(a == x && y + y == a){
+        System.out.println("a == x && y + y == a");
+
+    }
     }
 }
